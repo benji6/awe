be64282c993651db017db929f37a746b84a15c81
benji6 <benhall22@hotmail.co.uk>
2015-01-31 12:11:16 +0000

    more modular

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
 var audioCtx = new AudioContext();
 
-var oscillator = audioCtx.createOscillator();
+var Oscillator = require ('./lib/Oscillator.js');
+var GainNode = require ('./lib/GainNode.js');
 
-var gainNode = audioCtx.createGain();
+var oscillator = Oscillator(audioCtx);
+var gainNode = GainNode(audioCtx);
+
 oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 
-
-oscillator.type = 'square';
-oscillator.frequency.value = 3000; // value in hertz
-oscillator.start();
-
-gainNode.gain.value = 0.1;
-
-
-oscillator.type = 'square';
-oscillator.frequency.value = 3000; // value in hertz
 oscillator.start();
