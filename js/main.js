dec6529a50d16c96bd97dbf94c093910e2e7bcb0
benji6 <benhall22@hotmail.co.uk>
2015-02-15 19:56:35 +0000

    capitalised synth directory as it is now a factory

--- js/main.js
+++ js/main.js
@@ -2,19 +2,16 @@ var audioContext = require('./lib/audioContext.js');
 
 var keyboard = require('./lib/keyboard/output.js');
 var sequencer = require('./lib/sequencer/controller.js');
-var Synth = require('./lib/synth/Controller.js');
+var Synth = require('./lib/Synth/Controller.js');
 
 var synth0 = Synth();
-var synth1 = Synth();
-
-//sequencer.play();
-
 synth0.connectViewTo(document.body);
 synth0.connect(audioContext.destination);
+window.synth0 = synth0;
 
+var synth1 = Synth();
 synth1.connectViewTo(document.body);
 synth1.connect(audioContext.destination);
-
-
-window.synth0 = synth0;
 window.synth1 = synth1;
+
+//sequencer.play();
