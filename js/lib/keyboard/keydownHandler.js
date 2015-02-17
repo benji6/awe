7b40804d0d99b7a020ef4598c1dc87811765601e
benji6 <benhall22@hotmail.co.uk>
2015-02-17 19:46:23 +0000

    restructure keyboard

--- js/lib/keyboard/keydownHandler.js
+++
@@ -1,9 +0,0 @@
-module.exports = (startChannels) =>
-  (e) => {
-    var freq = getFreq(e);
-    if (freq) {
-      startChannels.forEach(function(channel) {
-        channel(freq);
-      });
-    }
-  };
--- js/lib/keyboard/keyupHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (stopChannels) =>
-  (e) => {
-    var freq = getFreq(e);
-    stopChannels.forEach(function(channel) {
-      channel(freq);
-    });
-  };
