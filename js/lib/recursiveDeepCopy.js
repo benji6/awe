ef9baa3570e97a7bbc3efa36d27ebaf4ce57a47a
benji6 <benhall22@hotmail.co.uk>
2015-02-18 19:43:37 +0000

    different method for testing if object is an array

--- js/lib/recursiveDeepCopy.js
+++ js/lib/recursiveDeepCopy.js
@@ -3,14 +3,14 @@
 var recursiveDeepCopy = (obj) => {
   if (typeof obj === 'object') {
     var newObj;
-    if (Object.prototype.toString.call(obj) === "[object Array]") {
+    if (Array.isArray(obj)) {
       newObj = [];
       obj.forEach((element) => {
         newObj.push(recursiveDeepCopy(element));
       });
       return newObj;
     }
-    
+
     newObj = {};
     Object.keys(obj).forEach((key) =>
     newObj[key] = recursiveDeepCopy(obj[key]));
