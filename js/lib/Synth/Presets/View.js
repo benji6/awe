22d135b8eed84d0d5735ae3b11a20128a55c5285
benji6 <benhall22@hotmail.co.uk>
2015-02-21 16:44:01 +0000

    updated jsml, menu contained in a div

--- js/lib/Synth/Presets/View.js
+++ js/lib/Synth/Presets/View.js
@@ -23,88 +23,36 @@ module.exports = function (model, channels) {
 
   var jsml = {
     tag: "div",
-    callback: (element) => synthViewHolder = element,
-    children: [
-      {
-        tag: "input",
-        placeholder: "Preset Name",
-        callback: (element) => saveAsPresetInput = element
-      },
-      {
-        tag: "button",
-        count: 5,
-        text: (count) => buttonLabels[count],
-        callback: function (element, jsmlElement, count) {
-          element.onclick = () =>
-            channels[buttonLabels[count].toLowerCase()](saveAsPresetInput.value);
-        }
-      },
-      {
-        tag: "input",
-        placeholder: "Paste Import Data Here",
-        callback: (element) =>
-          channels.import = () => {
-            channels.importdata(element.value);
-            element.value = "";
-          }
-      },
-      {
-        tag: "select",
-        callback: (element) => presetsSelectElement = element
-      },
-      {
-        tag: "div",
-        callback: (element) => channels.newNotification = (() => {
-          var cachedTimeoutId;
-
-          return (message) => {
-            window.clearTimeout(cachedTimeoutId);
-
-            var output = element.firstChild;
-
-            output.value = message;
-            cachedTimeoutId = window.setTimeout(function () {
-              output.value = "";
-            }, 5000);
-          };
-        })(),
-        children: {
-          tag: "output",
-          width: "200px"
-        }
-      }
-    ]
-  };
-
-  var jsml = {
-    tag: "menu",
-    text: "Menu",
-    children: [
-      {
-        tag: "menuitem",
-        text: "Open Preset"
-      },
-      {
-        tag: "menuitem",
-        text: "Save Preset As"
-      },
-      {
-        tag: "menuitem",
-        text: "Import Preset"
-      },
-      {
-        tag: "menuitem",
-        text: "Export Preset"
-      },
-      {
-        tag: "menuitem",
-        text: "Delete Preset"
-      },
-      {
-        tag: "menuitem",
-        text: "Initialize Settings"
-      },
-    ]
+    children: {
+      tag: "menu",
+      text: "Menu",
+      children: [
+        {
+          tag: "menuitem",
+          text: "Open Preset"
+        },
+        {
+          tag: "menuitem",
+          text: "Save Preset As"
+        },
+        {
+          tag: "menuitem",
+          text: "Import Preset"
+        },
+        {
+          tag: "menuitem",
+          text: "Export Preset"
+        },
+        {
+          tag: "menuitem",
+          text: "Delete Preset"
+        },
+        {
+          tag: "menuitem",
+          text: "Initialize Settings"
+        },
+      ]
+    }
   };
 
   channels.populatePresetsSelectList = populatePresetsSelectList;
