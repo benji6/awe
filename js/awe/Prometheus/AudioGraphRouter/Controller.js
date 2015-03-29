bb7f4d56260a8a6de7b4e921143ca8fed5b79e49
benji6 <benhall22@hotmail.co.uk>
2015-03-29 11:34:13 +0100

    more ramda

--- js/awe/Prometheus/AudioGraphRouter/Controller.js
+++ js/awe/Prometheus/AudioGraphRouter/Controller.js
@@ -14,10 +14,10 @@ module.exports = function () {
     return TypeToNodeFactoryMap[modelNode.type](modelNode.params);
   });
 
-  audioGraphModel.model.forEach(function (modelNode, index, array) {
+  R.forEachIndexed(function (modelNode, index, array) {
     const inputs = modelNode.inputs;
 
-    R.both(R.always(modelNode.eventListeners), R.forEach(function (event) {
+    R.both(R.identity, R.forEach(function (event) {
       switch (event) {
         case "noteStart":
           eventListeners.noteStart.push(nodes[index]);
@@ -26,24 +26,28 @@ module.exports = function () {
         }
     }))(modelNode.eventListeners);
 
-    const connectionKeys = R.both(
+    const connectionsKeys = R.both(
       R.identity,
       function (connections) {
         return Object.keys(connections);
       }
     )(modelNode.connections);
 
-    R.both(R.always(connectionKeys), R.forEach(function (connectionKey) {
+    R.both(R.identity, R.forEach(function (connectionKey) {
       nodes[index].connect(nodes[connectionKey].destinations[modelNode.connections[connectionKey]]);
-    }))(connectionKeys);
+    }))(connectionsKeys);
 
-    if (inputs) {
-      Object.keys(inputs).forEach(function (key) {
-        var value = inputs[key];
-        nodes[index].inputs[key](nodes[value]);
-      });
-    }
-  });
+    const inputsKeys = R.both(
+      R.identity,
+      function (connections) {
+        return Object.keys(connections);
+      }
+    )(modelNode.inputs);
+
+    R.both(R.identity, R.forEach(function (key) {
+      nodes[index].inputs[key](nodes[inputs[key]]);
+    }))(inputsKeys);
+  }, audioGraphModel.model);
 
   var connect = function (destination) {
     nodes[0].connect(destination);
