dccb9b791dd688b9634f6f825e36fa4ac6aa9a97
benji6 <benhall22@hotmail.co.uk>
2015-03-29 20:10:51 +0100

    remove reference to no longer extant file

--- js/awe/Prometheus/typeToNodeFactoryMap.js
+++ js/awe/Prometheus/typeToNodeFactoryMap.js
@@ -2,6 +2,5 @@ module.exports = {
   adsr: require('./Adsr/Controller.js'),
   filter: require('./Filter/Controller.js'),
   master: require('./Master/Controller.js'),
-  stereoPanner: require('./Panner/Controller.js'),
   oscillator: require('./Oscillator/Controller.js')
 };
