6d514975d2be7dbb03f0f6722af32101bb65c3b1
benji6 <benhall22@hotmail.co.uk>
2015-03-31 07:14:04 +0100

    swap jsml out of createRangeControl and replace with virtual-dom

--- js/awe/Components/createRangeControl.js
+++ js/awe/Components/createRangeControl.js
@@ -1,4 +1,5 @@
-var jsmlParse = require('jsml-parse');
+const h = require('virtual-dom/h');
+const createElement = require('virtual-dom/create-element');
 const R = require('ramda');
 
 const PRECISION = 12;
@@ -7,19 +8,19 @@ const capitalizeFirst = function (str) {
   return R.concat(R.toUpper(R.charAt(0, str)), R.slice(1, R.length(str), str));
 };
 
-var formatOutput = function (output) {
-  return (+output).toFixed(2);
+const formatOutput = function (output) {
+  return Number(output).toFixed(2);
 };
 
-var log12 = function (x) {
+const log12 = function (x) {
   return Math.log(x) / Math.log(12);
 };
 
-var exp12 = function (x) {
+const exp12 = function (x) {
   return Math.pow(12, x);
 };
 
-var maybe = function (callback) {
+const maybe = function (callback) {
   return function (boo) {
     return function (x) {
       if (boo) {
@@ -31,69 +32,41 @@ var maybe = function (callback) {
 };
 
 module.exports = function (params) {
-  var input = null;
-  var output = null;
-  var rootNode = null;
-  var maybeExp = maybe(exp12)(params.logarithmic);
-  var maybeLog = maybe(log12)(params.logarithmic);
-  var max = maybeLog(params.max);
-  var min = maybeLog(params.min);
+  const maybeExp = maybe(exp12)(params.logarithmic);
+  const maybeLog = maybe(log12)(params.logarithmic);
 
-  var modelValue = maybeLog(params.model[params.name]);
+  const rootNode = createElement(h("tr"));
+  const label = createElement(h("td", capitalizeFirst(params.name)));
+  const input = createElement(h("td", [
+    h("input", {
+      type: "range",
+      max: maybeLog(params.max),
+      min: maybeLog(params.min),
+      step: (params.step || (maybeLog(params.max) - maybeLog(params.min)) / 100).toPrecision(PRECISION),
+      value: maybeLog(params.model[params.name]),
+      oninput: function () {
+        oninputCallback(this.value);
+      }
+    })
+  ]));
+  const output = createElement(h("output", {
+    value: formatOutput(maybeExp(maybeLog(params.model[params.name])))
+  }));
 
-  var render = function () {
+  const render = function () {
     var modelValue = params.model[params.name];
     input.value = maybeLog(modelValue);
     output.value = formatOutput(modelValue);
   };
 
-  var oninputCallback = function (inputValue) {
-    var processedValue = maybeExp(inputValue);
-
-    params.observer[params.name](processedValue);
-    output.value = formatOutput(processedValue);
+  const oninputCallback = function (inputValue) {
+    params.observer[params.name](maybeExp(inputValue));
+    output.value = formatOutput(maybeExp(inputValue));
   };
 
-  var jsml = {
-    tag: "tr",
-    callback: function (element) {
-      rootNode = element;
-    },
-    children: [
-      {
-        tag: "td",
-        text: capitalizeFirst(params.name)
-      },
-      {
-        tag: "td",
-        children: {
-          tag: "input",
-          type: "range",
-          max: max,
-          min: min,
-          step: (params.step || (max - min) / 100).toPrecision(PRECISION),
-          value: modelValue,
-          callback: function (element) {
-            input = element;
-            element.oninput = function () {
-              oninputCallback(input.value);
-            };
-          }
-        }
-      },
-      {
-        tag: "td",
-        children: {
-          tag: "output",
-          value: formatOutput(maybeExp(modelValue)),
-          callback: function (element) {
-            output = element;
-          }
-        }
-      }
-    ]
-  };
-  jsmlParse(jsml, params.parent);
+  params.parent.appendChild(rootNode).appendChild(label);
+  rootNode.appendChild(input);
+  rootNode.appendChild(createElement(h("tr")).appendChild(output));
 
   return {
     render: render,
