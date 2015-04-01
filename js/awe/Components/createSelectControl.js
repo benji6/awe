914c1eafc688205675531d34e41f803583fc8430
benji6 <benhall22@hotmail.co.uk>
2015-04-01 07:24:34 +0100

    replace jsml-parse with virtual-dom

--- js/awe/Components/createSelectControl.js
+++ js/awe/Components/createSelectControl.js
@@ -1,4 +1,5 @@
-var jsmlParse = require('jsml-parse');
+const h = require('virtual-dom/h');
+const createElement = require('virtual-dom/create-element');
 const R = require('ramda');
 
 const capitalizeFirst = function (str) {
@@ -6,62 +7,36 @@ const capitalizeFirst = function (str) {
 };
 
 module.exports = function (params) {
-  var modelType = params.model.type;
-  var select;
+  const modelType = params.model.type;
 
-  var render = function () {
-    var modelParam = params.model[params.name];
-    var options = select.children;
+  const render = function () {
+    const modelParam = params.model[params.name];
+    const options = select.children;
 
-    for (var i = 0; i < options.length; i++) {
-      if (options[i].value === modelParam) {
-        options[i].selected = true;
+    R.forEach(function (option) {
+      if (option.value === modelParam) {
+        option.selected = true;
       } else {
-        options[i].selected = false;
+        option.selected = false;
       }
+    }, options);
+  };
+
+  const select = h("select", onchange = function () {
+    params.observer[params.name](select.value);
+  }, R.map(function (option) {
+    if (params.name === modelType) {
+      return h("option", {
+        selected: true
+      }, option);
     }
-  };
+    return h("option", option);
+  } ,params.options));
 
-  var createOptions = function () {
-    return params.options.map(function (option) {
-      var jsmlChild = {
-        tag: "option",
-        text: option,
-        value: option
-      };
-
-      if (params.name === modelType) {
-        jsmlChild.selected = true;
-      }
-
-      return jsmlChild;
-    });
-  };
-
-  var jsml = {
-    tag: "tr",
-    children: [
-      {
-        tag: "td",
-        text: capitalizeFirst(params.name)
-      },
-      {
-        tag: "td",
-        children: {
-          tag: "select",
-          children: createOptions(),
-          callback: function (element) {
-            select = element;
-            element.onchange = function () {
-              params.observer[params.name](select.value);
-            };
-          }
-        }
-      }
-    ]
-  };
-  jsmlParse(jsml, params.parent);
-
+  params.parent.appendChild(createElement(h("tr", [
+    h("td", capitalizeFirst(params.name)),
+    select
+  ])));
 
   return {
     render: render
