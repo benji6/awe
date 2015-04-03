70cb9191a9e9c2adb7c3cba47b30d2b07b998d77
benji6 <benhall22@hotmail.co.uk>
2015-04-03 14:43:02 +0100

    some tidying

--- js/awe/Prometheus/Controller.js
+++ js/awe/Prometheus/Controller.js
@@ -4,7 +4,7 @@ const model = require('./defaultModels/default.js');
 const Menu = require('./Menu/Controller.js');
 
 module.exports = function (parentDestination, parentDomElement, addStartChannel, addStopChannel) {
-  const container = view("Prometheus").connect(parentDomElement);
+  const container = view("Prometheus", parentDomElement);
   const prometheusBound = prometheusUnbound(parentDestination, container, addStartChannel, addStopChannel);
 
   Menu(prometheusBound, model).view.connect(container);
--- js/awe/Prometheus/view.js
+++ js/awe/Prometheus/view.js
@@ -1,17 +1,11 @@
 const h = require('virtual-dom/h');
 const createElement = require('virtual-dom/create-element');
-var R = require('ramda');
+const R = require('ramda');
 
-module.exports = function (pluginName) {
-  return {
-    connect: function (parentDomEl) {
-      const center = createElement(h("div.center"), [
-        h("h2", pluginName.to)
-      ]);
-      const synthContainer = createElement(h(`div.${R.toLower(pluginName)}`));
-      center.appendChild(synthContainer);
-      parentDomEl.appendChild(center);
-      return synthContainer;
-    }
-  };
+module.exports = function (pluginName, parenDomElement) {
+  return parenDomElement
+    .appendChild(createElement(h("div.center")))
+    .appendChild(createElement(h(`div.${R.toLower(pluginName)}`, [
+      h("h2", pluginName)
+    ])));
 };
