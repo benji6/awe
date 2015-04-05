9b1cb3c2f906b779e42415440fa0ec3b1ce5cb81
benji6 <benhall22@hotmail.co.uk>
2015-04-05 09:17:35 +0100

    fix save bug

--- js/awe/Prometheus/Menu/LocalStorageController.js
+++ js/awe/Prometheus/Menu/LocalStorageController.js
@@ -54,7 +54,7 @@ module.exports = function (pluginName) {
 
   //clear out any data stored by previous versions
   try {
-    R.either(getDecompressAndParse(), localStorage.clear());
+    R.either(getDecompressAndParse, localStorage.clear)();
   }
   catch (e) {
     localStorage.clear();
